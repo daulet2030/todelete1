84ee564b77ef69c4c1d0f03c8a3cb37684542092
daulet2030 <daulet2030@gmail.com>
2021-03-08 21:35:41 +0600

    Initial commit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,29 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Test1 {
+    @Test
+    public void test1() {
+
+    }
+
+    @Test
+    public void test2() {
+
+    }
+
+    @Test
+    public void test3() {
+        Assert.fail();
+    }
+
+    @Test
+    public void test4() {
+
+    }
+
+    @Test
+    public void test5() {
+
+    }
+}
